08c80bc772bd48ce9b9ac3df71df56af02a72df4
Jan Kammerath <jankammerath@gmail.com>
2025-07-07 22:16:27 +0200

    Add newline at end of build.sh and format comments for clarity

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# build the local thing
 go build -o bin/gobbench main.go
+
+# build for AWS Graviton2
+GOOS=linux GOARCH=arm64 go build -o bin/gobbench-graviton2 main.go
+
+# build for IBM z/Architecture (LinuxONE)
 GOOS=linux GOARCH=s390x go build -o bin/gobbench-s390x main.go
